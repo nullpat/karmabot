60e959600d381a6f20d553b99972d7de8aae1b78
ppatkowski@onsolve.com <patryk.patkowski@onsolve.com>
2018-09-12 16:39:47 -0400

    help text

--- commands/help.py
+++ commands/help.py
@@ -1,5 +1,5 @@
 def create_commands_table(commands):
-    """Print this help text"""
+    """Help for Templars who have lost their way"""
     ret = '\n'.join(['{:<30}: {}'.format(name, func.__doc__)
                      for name, func in sorted(commands.items())])
     return '```{}```'.format(ret)
